32dd2186d7e605a1ae41b7ecb46b347a62bc11b5
Alireza <arbor0711@gmail.com>
2023-05-08 18:13:49 +0200

    Initial commit Navbar component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Homepage from "./components/Homepage"
+import AboutMe from "./components/AboutMe"
+import './App.css' 
+
+function App(){
+  return(
+    <>
+    <nav className="nav">
+      <a href="#">Home</a>
+      <a href="#">About me</a>
+    </nav>
+    <Homepage />
+    <AboutMe />
+    </>
+  )
+}
+
+export default App
